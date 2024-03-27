c8dacf8172485220c27e863a00a44067a4def0a2
Hossam Ramadan <103776358+HossamRamadan667@users.noreply.github.com>
2024-03-28 00:58:09 +0200

    activate routes
    index.js: just correct css file import line to override on bootstrap

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import { Route, Routes } from 'react-router-dom';
+import Header from './Components/Header';
+import Home from './views/Home';
+import Services from './views/Services';
+import Contact from './views/Contact';
+import About from './views/About';
 
-// ? info: 
-// ? just remove comments when create and import the elements
 
 function App() {
   return (
     <>
-      {/* <Navbar/> */}
+      <Header />
       <Routes>
-        {/* <Route path='/' element={<Home></Home>} />
-        <Route path='/services' element={<Services></Services>} />
-        <Route path='/about' element={<About></About>} />
-        <Route path='/contact' element={<Contact></Contact>} /> */}
+        <Route path='/' element={<Home />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
       </Routes>
     </>
   );
