f1d9aba8f494c7ab45ae62ebedb573fff0ed4157
Hossam Ramadan <103776358+HossamRamadan667@users.noreply.github.com>
2024-03-30 02:13:31 +0200

    create and style header
    remove useless css file

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,39 @@
 import Navbar from "./Navbar";
+import logo from '../logo.svg'
 
 function Header() {
     return (
         <header>
+            <div className="container py-3">
+                <div className="row justify-content-center justify-content-md-start">
 
-            <Navbar />
+                    <div className="col-4 col-md-3 d-flex flex-row align-items-center">
+                        <div className="logo me-2">
+                            <img src={logo} alt="logo" />
+                        </div>
+                        <h5 className="mb-0">ENERGYM</h5>
+                    </div>
+
+                    <div className="row col-3 d-none d-md-flex">
+                        <div className="d-flex align-items-center">
+                            <i class="fa-solid fa-location-dot fa-2x"></i>
+                            <p>helwan</p>
+                        </div>
+                    </div>
+                    <div className="row col-3 d-none d-md-flex">
+                        <div className="d-flex align-items-center">
+                            <i class="fa-solid fa-phone fa-2x"></i>
+                            <p>01148183794</p>
+                        </div></div>
+                    <div className="row col-3 d-none d-md-flex">
+                        <div className="d-flex align-items-center">
+                            <i class="fa-solid fa-envelope fa-2x"></i>
+                            <p>email@Gmail.com</p>
+                        </div></div>
+                </div>
+                <Navbar />
+            </div>
+
         </header>);
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,30 @@
+import { NavLink } from "react-router-dom";
+
 function Navbar() {
     return (
-        <nav>
-            
+        <nav className="navbar navbar-expand-lg navbar-light   justify-content-center  ">
+            <button className="navbar-toggler mb-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fa-solid fa-bars fa-2x"></i>
+            </button>
+
+            <div className="collapse navbar-collapse  justify-content-center mx-3 " id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item  ">
+                        <NavLink className="nav-link" to="/">Home  </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/about">About</NavLink>
+                    </li>
+
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/services">Services</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/contact">Contact</NavLink>
+                    </li>
+                </ul>
+
+            </div>
         </nav>
     );
 }
